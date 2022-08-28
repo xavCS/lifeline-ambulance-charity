6ec418fbaf105342300a751e2dd78051fbcdac48
Xavier <80826662+xavCS@users.noreply.github.com>
2022-08-28 15:15:46 +0100

    Added global stylesheet

--- events.js
+++ events.js
@@ -1,13 +1,34 @@
-// const mobile = window.matchMedia('(max-width: 1260px)');
-
 const hamMenu = document.querySelector('#hamburger-menu');
 
 hamMenu.addEventListener('click', ()=>{
     const pages = document.querySelector('#page-links');
     pages.classList.toggle('unhide');
-    
-    // const content = document.querySelectorAll('.all')
-    // for ( section of content) {
-    //     section.classList.toggle('blur');
-    // }
+})
+
+// const sliderOne = document.getElementById('slider-one');
+// const sliderTwo = document.getElementById('slider-two');
+// const sliderThree = document.getElementById('slider-three');
+
+// sliderOne.addEventListener('click', () => {
+//     sliderOne.classList.add('active');
+//     sliderTwo.classList.remove('active');
+//     sliderThree.classList.remove('active');
+// })
+
+// sliderTwo.addEventListener('click', () => {
+//     sliderTwo.classList.add('active');
+//     sliderOne.classList.remove('active');
+//     sliderThree.classList.remove('active');
+// })
+
+// sliderThree.addEventListener('click', () => {
+//     sliderThree.classList.add('active');
+//     sliderOne.classList.remove('active');
+//     sliderTwo.classList.remove('active');
+// })
+
+const image = document.querySelector('img');
+
+image.addEventListener('click', () => {
+    image.classList.toggle('active')
 })
